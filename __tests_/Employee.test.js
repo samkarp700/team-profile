f9d38dc4f17bad045a8ab44f6ceb6d58e4e3a7f9
Sam Karp <sfcypret@gmail.com>
2022-08-13 20:05:16 -0400

    added lib files, began test developement

---
+++ __tests_/Employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates container for employee info', () => {
+    const Employee = new Employee();
+
+    //expect team manager name
+    //expect employee ID
+    //expect email address
+    //expect office number
+})
+
