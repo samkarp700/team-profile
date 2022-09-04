d6efdaa105985ca404f84947ae05f0721718a7e8
Sam Karp <sfcypret@gmail.com>
2022-09-04 10:17:32 -0400

    packages

--- __tests_/Employee.test.js
+++ __tests_/Employee.test.js
@@ -1,11 +1,15 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates container for employee info', () => {
-    const Employee = new Employee();
+    const Employee = new Employee('Julie');
 
     //expect team manager name
+    expect(employee.name).toBe('Julie');
     //expect employee ID
+    expect(employee.id).toEqual(expect.any(Number));
     //expect email address
+    expect(employee.email).toBe('julie@email.com');
     //expect office number
-})
+    expect(employee.offnumber).toEqual(expect.any(Number));
+});
 
