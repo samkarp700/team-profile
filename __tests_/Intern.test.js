2db60f4b77daf2d1bbbfb35321918b3a9145761a
Sam Karp <sfcypret@gmail.com>
2022-09-04 13:36:53 -0400

    intern container test passed

---
+++ __tests_/Intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require('../lib/Intern');
+
+test('creates a container for Intern info', () => {
+    //must inherit Employee info + school
+const intern = new Intern('Callie', 4, 'callie@intern.com', 'University of Central Florida');
+
+ //expect intern name
+ expect(intern.name).toEqual(expect.any(String));
+ //expect intern ID
+ expect(intern.id).toEqual(expect.any(Number));
+ //expect email address
+ expect(intern.email).toEqual(expect.any(String));
+ //expect school as string
+ expect(intern.schoolName).toEqual(expect.any(String));
+
+ console.log(`Intern Name: ${intern.name}`);
+ console.log(`Intern ID: ${intern.id}`);
+ console.log(`Intern Email: ${intern.email}`);
+ console.log(`Intern School: ${intern.schoolName}`);
+});
