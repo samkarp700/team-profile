73ae84de94dc816d2042162d94b5a0b5ff47513b
Sam Karp <sfcypret@gmail.com>
2022-09-04 14:55:06 -0400

    generate site created

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const inquirer = require('inquirer');
 
+
+// employee containers
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
---
+++ src/generatesite.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('.dist/index.html', fileContent, err => {
+            //if error - reject promise
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+            }
+            resolve({
+                ok: true, 
+                message: 'Style sheet copied!'
+            });
+        });
+    });
+};
+
+module.exports = {writeFile, copyFile};
